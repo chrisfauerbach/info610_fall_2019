7a5c86aa19ca961202e4f6c104d745116d0b99c5
Chris Fauerbach <chrisfauerbach@gmail.com>
2019-11-14 19:33:19 -0500

    adding myscript.sh to show example

---
+++ week12/myscript.sh
@@ -0,0 +1,5 @@
+curl http://localhost:5000/country
+curl "http://localhost:5000/country?name=Austria"
+curl "http://localhost:5000/country?name=Austria"
+curl "http://localhost:5000/country?name=Austria"
+curl "http://localhost:5000/country/9"
